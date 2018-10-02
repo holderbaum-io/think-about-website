53a96cde285cd31b3eedb8caaf0cf238f0d7cd6c
Jakob Holderbaum <jakob@holderbaum.io>
2018-10-02 11:02:04 +0200

    Add basic templating

--- lib/server.rb
+++ lib/server.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'erb'
 
 class Result
   attr_reader :content, :type
@@ -7,6 +8,30 @@ class Result
   def initialize(content, type)
     @content = content
     @type = type
+  end
+end
+
+class TemplateValues
+  attr_reader :content
+
+  def initialize(content)
+    @content = content
+  end
+
+  def title
+    'lol'
+  end
+
+  def _context
+    binding
+  end
+end
+
+class PageValues
+  attr_reader :content
+
+  def _context
+    binding
   end
 end
 
@@ -21,7 +46,6 @@ end
 
 class Server < Sinatra::Application
   def resolve_path(file)
-    p :resolve_path => file
     return file if File.file? file
     return resolve_path(file + '.erb') if File.file? file + '.erb'
     return resolve_path(File.join(file, 'index.html')) if File.directory? file
@@ -41,13 +65,30 @@ class Server < Sinatra::Application
     end
   end
 
-  def render(file)
-    Result.new(File.read(file), resolve_type(file))
+  def render_page(file)
+    if File.extname(file) == '.erb'
+      template = ERB.new(File.read(file))
+      values = PageValues.new
+      template.result(values._context)
+    else
+      File.read(file)
+    end
+  end
+
+  def render_document(file)
+    type = resolve_type(file)
+    if type == 'text/html'
+      template = ERB.new(File.read('template.html.erb'))
+      values = TemplateValues.new(render_page file)
+      Result.new(template.result(values._context), type)
+    else
+      Result.new(render_page(file), type)
+    end
   end
 
   get '/*' do
     begin
-      result = render(resolve_path(params[:splat].first))
+      result = render_document(resolve_path(params[:splat].first))
       content_type result.type
       result.content
     rescue ServerError => e
