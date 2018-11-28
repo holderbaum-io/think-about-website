2a214f677ea07bdfcd04845b074233a156a82e8f
Jakob Holderbaum <jakob@holderbaum.io>
2018-11-28 21:52:16 +0100

    Restructure site to contain an english-only blog

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 gem 'faraday'
 gem 'kramdown'
+gem 'kramdown-metadata-parsers'
 gem 'sinatra'
 gem 'sinatra-contrib'
--- lib/builder.rb
+++ lib/builder.rb
@@ -5,23 +5,35 @@ class Builder
   def initialize(source_dir, target_dir)
     @source_dir = source_dir
     @target_dir = target_dir
+    @renderer = Renderer.new(@source_dir)
   end
 
   def run
-    renderer = Renderer.new(@source_dir)
     languages = %w[en de]
 
     languages.each do |lang|
-      Dir['pages/**'].each do |page|
-        file = page.gsub(/\.erb$/, '').gsub(%r{^pages/}, '')
-        file = '' if file == 'index.html'
-        result = renderer.render(lang, file)
-        target_path = File.join(@target_dir, lang, result.filename)
-        FileUtils.mkdir_p(File.dirname(target_path))
-        File.write(target_path, result.content)
+      Dir['pages/*'].each do |page|
+        next if File.directory? page
+        build_page File.join(@target_dir, lang), lang, page
       end
     end
 
+    Dir['pages/blog/*'].each do |page|
+      next if File.directory? page
+      build_page @target_dir, 'en', page.gsub(/\.md$/, '.html')
+    end
+
     FileUtils.cp_r(File.join(@source_dir, 'assets'), @target_dir)
+  end
+
+  private
+
+  def build_page(target_dir, lang, page)
+    file = page.gsub(/\.erb$/, '').gsub(%r{^pages/}, '')
+    file = '' if file == 'index.html'
+    result = @renderer.render(lang, file)
+    target_path = File.join(target_dir, result.filename)
+    FileUtils.mkdir_p(File.dirname(target_path))
+    File.write(target_path, result.content)
   end
 end
